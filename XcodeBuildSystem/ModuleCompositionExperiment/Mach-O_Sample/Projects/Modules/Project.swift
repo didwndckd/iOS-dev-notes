d9b8bd7654c81ea7995b93b4f670e40bbef3464c
didwndckd <didwndckd@gmail.com>
2026-08-01 04:13:11 +0900

    불필요한 파일 제거

--- XcodeBuildSystem/ModuleCompositionExperiment/Mach-O_Sample/Projects/Modules/Project.swift
+++
@@ -1,7 +0,0 @@
-@preconcurrency import ProjectDescription
-import ProjectDescriptionHelpers
-
-let project = Project(
-    name: "Modules",
-    targets: Targets.allCases.map { $0.moduleTarget }// + Targets.allCases.map { $0.appTarget }
-)
